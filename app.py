16a41bdd75a90980e349b093e589d8e2ab533543
Guilherme <guilherme.valdelino@academico.ifpb.edu.br>
2024-10-01 15:57:15 -0300

    quarta commit

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from usuarios import add_user, authenticate_user
 from cadastro_clubes import add_clube, get_clubes
+import os
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = 'static/uploads/clubes'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "chave secreta"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def projetoEscola():
@@ -70,6 +76,9 @@ def informacoesdoclube(nome_clube):
         return render_template("clubes/clubes_informacoes.html", clube=clube)
     return "Clube não encontrado", 404
 
+def allowed_file(filename):
+    """Verifica se o arquivo é de um tipo permitido."""
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/cadastro_clube", methods=['POST'])
 def cadastro_clube():
@@ -77,14 +86,24 @@ def cadastro_clube():
     lider = request.form.get('lider')
     vice_lider = request.form.get('vice_lider')
     contato = request.form.get('contato')
-    descricao = request.form.get('descricao')  # Novo campo de descrição
-    
-    # Adicione o novo campo 'descricao' ao método `add_clube`
-    if add_clube(nome_clube, lider, vice_lider, contato, descricao):
+    descricao = request.form.get('descricao')
+
+    if 'foto_clube' not in request.files:
+        return render_template("clubes/cadastrar_clubes.html", mensagem_erro_clube="Nenhuma foto foi enviada")
+
+    foto_clube = request.files['foto_clube']
+
+
+    if foto_clube.filename == '' or not allowed_file(foto_clube.filename):
+        return render_template("clubes/cadastrar_clubes.html", mensagem_erro_clube="Arquivo inválido ou não enviado")
+
+    filename = secure_filename(foto_clube.filename)
+    foto_clube.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    if add_clube(nome_clube, lider, vice_lider, contato, descricao, filename):
         return redirect(url_for("telaClubes"))
     else:
         return render_template("clubes/cadastrar_clubes.html", mensagem_erro_clube="Clube já cadastrado")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
